dd23524c0e7a8a4373330dcea0398677a3dad807
Parkseojin <seojin5565@gmail.com>
2024-05-19 03:43:51 +0900

    [Feat] - #7 서비스구현

--- src/main/java/com/sopt/sopkathon/service/AnswerServiceImpl.java
+++ src/main/java/com/sopt/sopkathon/service/AnswerServiceImpl.java
@@ -3,6 +3,7 @@ package com.sopt.sopkathon.service;
 import com.sopt.sopkathon.repository.AnswerRepository;
 import com.sopt.sopkathon.service.dto.response.AnswerListResponseDto;
 import com.sopt.sopkathon.service.dto.response.AnswerResponseDto;
+import com.sopt.sopkathon.service.dto.response.AnswerWeightResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,5 +21,11 @@ public class AnswerServiceImpl implements AnswerService{
 
     // memberService.findMemberById(memberId);
     return AnswerListResponseDto.of(answerRepository.findByQuestionId(questionId));
+  }
+
+  @Transactional(readOnly = true)
+  @Override
+  public AnswerWeightResponseDto findAnswerWeight(final Long memberId, Long answerId) {
+    return answerRepository.getWeightByAnswerId(answerId);
   }
 }
