c3ba57cd549118f7a5bc36f8bd6bb25a097e9fad
Parkseojin <seojin5565@gmail.com>
2024-05-19 03:43:46 +0900

    [Feat] - #7 레포지토리 구현

--- src/main/java/com/sopt/sopkathon/repository/AnswerRepositoryCustom.java
+++ src/main/java/com/sopt/sopkathon/repository/AnswerRepositoryCustom.java
@@ -2,9 +2,12 @@ package com.sopt.sopkathon.repository;
 
 import com.sopt.sopkathon.domain.Answer;
 import com.sopt.sopkathon.service.dto.response.AnswerResponseDto;
+import com.sopt.sopkathon.service.dto.response.AnswerWeightResponseDto;
 import java.util.List;
 
 public interface AnswerRepositoryCustom {
 
   List<AnswerResponseDto> findByQuestionId(Long questionId);
+
+  AnswerWeightResponseDto getWeightByAnswerId (Long answerId);
 }
